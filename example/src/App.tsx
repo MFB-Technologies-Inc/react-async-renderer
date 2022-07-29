7efa0fcace97d2c1416d09d00e28e12a9299c05a
Ethan Lemke <ethan@mfbtech.com>
2022-07-29 12:46:30 -0500

    example: update App and add an example of the loading spinner

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,18 @@
 // Copyright 2022 MFB Technologies, Inc.
 
-import logo from './logo.svg';
 import './App.css';
+import * as reactAsyncRenderer from "@mfbtech/react-async-renderer"
 
 export function App() {
+  const { LoadingSpinner } = reactAsyncRenderer
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>@mfbtech/react-async-renderer</h1>
+      <h2>Loading spinner example</h2>
+      <div>
+        <LoadingSpinner />
+      </div>
     </div>
   );
 }
