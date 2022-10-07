e63cac2fe852797205924bc7cff1dfa933a411c2
Ethan Lemke <lemke.ethan@gmail.com>
2022-10-07 16:39:10 +0000

    .eslintrc.js: remove rush specific stuff, remove unused typescript eslint packages and extend react-app eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
-// This is a workaround for https://github.com/eslint/eslint/issues/3458
-require("@rushstack/eslint-config/patch/modern-module-resolution")
+// Copyright 2022 MFB Technologies, Inc.
 
 module.exports = {
   extends: [
-    "@rushstack/eslint-config/profile/web-app",
-    "@rushstack/eslint-config/mixins/react"
+    "react-app"
   ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/consistent-type-definitions": [1, "type"],
-    "@typescript-eslint/typedef": 0,
-    "@rushstack/no-new-null": 0,
-    "@rushstack/typedef-var": 0
-  },
-  parserOptions: { tsconfigRootDir: __dirname },
-  settings: {
-    react: {
-      version: "17.0"
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/consistent-type-definitions": [1, "type"],
+        "@typescript-eslint/typedef": 0,
+      }
     }
-  }
+  ]
 }
