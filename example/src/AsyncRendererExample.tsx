7e6527da6d013dd3c1f6a7aa95a2f03e7ad066fd
Ethan Lemke <lemke.ethan@gmail.com>
2022-12-17 17:00:44 +0000

    update the example app to use the new async status typing

--- example/src/AsyncRendererExample.tsx
+++ example/src/AsyncRendererExample.tsx
@@ -1,50 +1,76 @@
 // Copyright 2022 MFB Technologies, Inc.
 
-import { AsyncRequestStatus, createAsyncRenderer } from "@mfbtech/react-async-renderer"
+import {
+  AsyncRequestStatus,
+  AsyncRequestStatusEnum,
+  createAsyncRenderer
+} from "@mfbtech/react-async-renderer"
 import { useState } from "react"
 import "./AsyncRendererExample.css"
 
 export function AsyncRendererExample() {
-  const [currentAsyncRequestState, setCurrentAsyncRequestState] = useState(AsyncRequestStatus.INIT)
-  const asyncRequestError = currentAsyncRequestState === AsyncRequestStatus.ERROR ? "some error occurred!" : null
-  const asyncRequestData = currentAsyncRequestState === AsyncRequestStatus.FULFILLED ? { data: "my data" } : null
+  const [currentAsyncRequestState, setCurrentAsyncRequestState] =
+    useState<AsyncRequestStatus>(AsyncRequestStatusEnum.INIT)
+  const asyncRequestError =
+    currentAsyncRequestState === AsyncRequestStatusEnum.ERROR
+      ? "some error occurred!"
+      : null
+  const asyncRequestData =
+    currentAsyncRequestState === AsyncRequestStatusEnum.FULFILLED
+      ? { data: "my data" }
+      : null
   const renderer = createAsyncRenderer({
     status: currentAsyncRequestState,
     error: asyncRequestError,
-    onCompletedSuccessfullyArgs: asyncRequestData,
+    onCompletedSuccessfullyArgs: asyncRequestData
   })
 
   return (
     <div className="async-renderer-example">
       <h2>Async renderer example</h2>
       <div>
-        <label htmlFor="asyncRequestStatus">Status of the long running process:</label>
+        <label htmlFor="asyncRequestStatus">
+          Status of the long running process:
+        </label>
         <select
           value={currentAsyncRequestState}
-          onChange={(e) => {
-            setCurrentAsyncRequestState(e.currentTarget.value as AsyncRequestStatus)
+          onChange={e => {
+            setCurrentAsyncRequestState(
+              e.currentTarget.value as AsyncRequestStatus
+            )
           }}
-          id="asyncRequestStatus">
-          {
-            Object.entries(AsyncRequestStatus).map(([name, value]) => {
-              return (
-                <option key={value} value={value}>{name}</option>
-              )
-            })
-          }
+          id="asyncRequestStatus"
+        >
+          {Object.entries(AsyncRequestStatusEnum).map(([name, value]) => {
+            return (
+              <option key={value} value={value}>
+                {name}
+              </option>
+            )
+          })}
         </select>
       </div>
       <div>
-        {renderer((args) => {
-          return (
-            <p className='renderer-result success'>{args.data}</p>
-          )
-        },
+        {renderer(
+          args => {
+            return <p className="renderer-result success">{args.data}</p>
+          },
           {
-            onInit: () => (<p className='renderer-result info'>Waiting for long running process to start...</p>),
-            onLoading: () => (<p className='renderer-result loading'>custom spinner...</p>),
-            onCompletedWithError: (props) => (<p className='renderer-result error'>{props.errorMessage ?? "error"}</p>)
-          })}
+            onInit: () => (
+              <p className="renderer-result info">
+                Waiting for long running process to start...
+              </p>
+            ),
+            onLoading: () => (
+              <p className="renderer-result loading">custom spinner...</p>
+            ),
+            onCompletedWithError: props => (
+              <p className="renderer-result error">
+                {props.errorMessage ?? "error"}
+              </p>
+            )
+          }
+        )}
       </div>
     </div>
   )
--- example/src/useLongRunningProcess.ts
+++ example/src/useLongRunningProcess.ts
@@ -1,11 +1,20 @@
 // Copyright 2022 MFB Technologies, Inc.
 
-import { AsyncRequestStatus } from "@mfbtech/react-async-renderer"
+import {
+  AsyncRequestStatus,
+  AsyncRequestStatusEnum
+} from "@mfbtech/react-async-renderer"
 import { useEffect, useState } from "react"
 
 /** starts a process that takes the specified amount of time and returns the specified data. */
-export function useLongRunningProcess<T>(processDurationInSeconds: number, processData: T, skip: boolean) {
-  const [status, setStatus] = useState<AsyncRequestStatus>(AsyncRequestStatus.INIT)
+export function useLongRunningProcess<T>(
+  processDurationInSeconds: number,
+  processData: T,
+  skip: boolean
+) {
+  const [status, setStatus] = useState<AsyncRequestStatus>(
+    AsyncRequestStatusEnum.INIT
+  )
   const [error, setError] = useState<null | string>(null)
   const [data, setData] = useState<null | T>(null)
   const [restart, setRestart] = useState(true)
@@ -17,25 +26,27 @@ export function useLongRunningProcess<T>(processDurationInSeconds: number, proce
     if (skip) {
       return
     }
-    setStatus(AsyncRequestStatus.PENDING)
-    startLongRunningProcess(processDurationInSeconds, processData).then(data => {
-      setError(null)
-      setStatus(AsyncRequestStatus.FULFILLED)
-      setData(data)
-    }).catch((err: unknown) => {
-      setData(null)
-      setStatus(AsyncRequestStatus.ERROR)
-      let errorMessage = "error"
-      if (typeof err === "string") {
-        errorMessage = err
-      }
-      else if (err instanceof Error) {
-        errorMessage = err.message
-      }
-      setError(errorMessage)
-    }).finally(() => {
-      setRestart(false)
-    })
+    setStatus(AsyncRequestStatusEnum.PENDING)
+    startLongRunningProcess(processDurationInSeconds, processData)
+      .then(data => {
+        setError(null)
+        setStatus(AsyncRequestStatusEnum.FULFILLED)
+        setData(data)
+      })
+      .catch((err: unknown) => {
+        setData(null)
+        setStatus(AsyncRequestStatusEnum.ERROR)
+        let errorMessage = "error"
+        if (typeof err === "string") {
+          errorMessage = err
+        } else if (err instanceof Error) {
+          errorMessage = err.message
+        }
+        setError(errorMessage)
+      })
+      .finally(() => {
+        setRestart(false)
+      })
   }, [processData, processDurationInSeconds, restart, skip])
 
   return {
@@ -43,7 +54,7 @@ export function useLongRunningProcess<T>(processDurationInSeconds: number, proce
     error,
     data,
     reset: () => {
-      setStatus(AsyncRequestStatus.INIT)
+      setStatus(AsyncRequestStatusEnum.INIT)
       setError(null)
       setData(null)
       setRestart(true)
@@ -51,10 +62,13 @@ export function useLongRunningProcess<T>(processDurationInSeconds: number, proce
   }
 }
 
-function startLongRunningProcess<T>(processDurationInSeconds: number, processData: T): Promise<T> {
-  return new Promise((resolve) => {
+function startLongRunningProcess<T>(
+  processDurationInSeconds: number,
+  processData: T
+): Promise<T> {
+  return new Promise(resolve => {
     setTimeout(() => {
       resolve(processData)
-    }, processDurationInSeconds);
+    }, processDurationInSeconds)
   })
 }
