6418f07b571e5b8d167b10c384e74a27fc8d61b1
Ethan Lemke <ethan@mfbtech.com>
2023-02-24 21:38:03 +0000

    scripts/package.js: fix formatting

--- scripts/package.js
+++ scripts/package.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 // Copyright 2022 MFB Technologies, Inc.
 
-const path = require("path");
-const fs = require("fs");
-const childProcess = require('child_process');
+const path = require("path")
+const fs = require("fs")
+const childProcess = require("child_process")
 
-const targetPackDirectory = path.join(__dirname, ".." ,"packed");
-const npmPackCommand = `npm pack --pack-destination ${targetPackDirectory}`;
+const targetPackDirectory = path.join(__dirname, "..", "packed")
+const npmPackCommand = `npm pack --pack-destination ${targetPackDirectory}`
 
-(function main() {
-  fs.mkdir(targetPackDirectory, { recursive: true }, (err) => {
-    if (err) throw err;
-  });
+;(function main() {
+  fs.mkdir(targetPackDirectory, { recursive: true }, err => {
+    if (err) throw err
+  })
   childProcess.execSync(npmPackCommand)
-})();
+})()
