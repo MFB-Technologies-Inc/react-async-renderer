111d08936d3e3b8c5541170110c0e2d90820ee35
Ethan Lemke <lemke.ethan@gmail.com>
2023-10-20 18:12:24 +0000

    ErrorReport.tsx, LoadingSpinner.tsx: use the `ComponentResult` type

--- src/components/ErrorReport.tsx
+++ src/components/ErrorReport.tsx
@@ -1,5 +1,7 @@
 // Copyright 2023 MFB Technologies, Inc.
 
+import { ComponentResult } from "../types"
+
 const text = {
   genericErrorMessage: "An error prevented rendering."
 }
@@ -8,7 +10,7 @@ export type ErrorReportProps = {
   message?: string
 }
 
-export function ErrorReport(props: ErrorReportProps) {
+export function ErrorReport(props: ErrorReportProps): ComponentResult {
   return (
     <div className="error-report">
       <p>{text.genericErrorMessage}</p>
